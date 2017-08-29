dc6abf9d879e5cdb3d433d2ba9e9b4a90a3394e7
Chirstian Hizon <ChirstianHizon@users.noreply.github.com>
2017-08-29 15:37:20 +0800

    Revert "Changed tbl_update_log to tbl_product_log and removed cust_id"
    This reverts commit 58dac8e00299e4e8656998efd29340246ff63204.

--- classes/class.product_log.php
+++ classes/class.product_log.php
@@ -15,11 +15,11 @@ class Product_Log{
 
 
   public function getAllProductLog(){
-    $sql = "SELECT log_id AS LOG_ID, tbl_product_log.prd_id AS PRD_ID,log_qty AS LOG_QTY, log_datestamp AS DATESTAMP,
+    $sql = "SELECT log_id AS LOG_ID, tbl_update_log.prd_id AS PRD_ID,log_qty AS LOG_QTY, log_datestamp AS DATESTAMP,
 		emp_first_name AS EMP_FNAME, emp_last_name AS EMP_LNAME, log_type AS TYPE
-    FROM tbl_product_log
-    INNER JOIN tbl_product ON tbl_product_log.prd_id = tbl_product.prd_id
-    INNER JOIN tbl_employee ON tbl_product_log.emp_id = tbl_employee.emp_id
+    FROM tbl_update_log
+    INNER JOIN tbl_product ON tbl_update_log.prd_id = tbl_product.prd_id
+    INNER JOIN tbl_employee ON tbl_update_log.emp_id = tbl_employee.emp_id
     ";
     $result = mysqli_query($this->db,$sql);
     if($result){

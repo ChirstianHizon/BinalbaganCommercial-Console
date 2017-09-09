42d18700f4b37f95eb6a97ea0cb76c084bdfe3d9
Ron Jaundel C. Guanzon <rjguanzon19@gmail.com>
2017-09-09 21:16:41 +0800

    login design

--- login.php
+++ login.php
@@ -12,7 +12,7 @@ if($employee->get_session()){
  ?>
 <!DOCTYPE html>
 <html>
-  <head>
+  <!-- <head>
     <title>BC Console Login</title>
 
   </head>
@@ -24,5 +24,35 @@ if($employee->get_session()){
   </form>
   <script src="js\login.js"></script>
   <script src="js\jquery.min.js"></script>
+  </body> -->
+  <head>
+    <title>Login</title>
+    <meta name="viewport" content="initial-scale=1.0">
+    <meta charset="utf-8">
+    <link rel="stylesheet" type="text/css" href=".main/stylesheet.css">
+    <link rel="stylesheet" type="text/css" href=".main/login.css">
+  </head>
+
+  <body>
+
+    <div id="navigation-container">
+
+    </div>
+    <div id="content-container">
+      <div class="form">
+        <div id="thumbnail"class="thumbnail">
+          <img src="images/logo.png"/>
+        </div>
+        <form class="login-form" id="loginform" action="" method="POST" name="login">
+          <input name="userid" id="uname"   type="text" placeholder="username"/>
+          <input name="password" id="pass" type="password" placeholder="password"/>
+          <button type="submit" name="submit">login</button>
+          <p class="message">Not registered? <a href="#">Create an account</a></p>
+        </form>
+      </div>
+    </div>
+    <script src="js\login.js"></script>
+    <script src="js\jquery.min.js"></script>
   </body>
+
 </html>

8ef9daf2d95c629a4d4879e1888dd138d3f66c0b
Chirstian Hizon <ChirstianHizon@users.noreply.github.com>
2017-09-09 22:56:18 +0800

    barcode

--- .admin/inventory/inventory-add-barcode.php
+++ .admin/inventory/inventory-add-barcode.php
@@ -12,41 +12,25 @@
     <div id="line"></div>
     <br/>
     <b>Barcode Name:</b></br>
-    <input id="name" type="text" required><br/><br/>
-    <b>Category Description:</b><br/>
-    <textarea id="desc" rows="8" required></textarea><br/><br/>
+    <input id="name" type="text" required>
     <button >Add New Barcode</button>
   </form>
 
-  <form id="updateCategory" class="formcat">
-    <h3>Update Category</h3>
-    <div id="line"></div>
-    <br/>
-    <input readonly='true' class="nodisplay" id="upid" type="numeric" required>
-    Category Name:<br/>
-    <input id="upname" type="text" required><br/><br/>
-    Category Description:<br/>
-    <textarea id="updesc" rows="8" required></textarea><br/><br/>
-    <button >Update Category</button>
-  </form>
   <br/>
   <button  id="clear" >Clear</button>
 </div>
 <div id="div2" class="col span_1_of_2">
-    <div id="tableidreborn">
     <table id="table_id" class="display" width="100%" cellspacing="0">
       <thead>
           <tr>
               <th>Name</th>
-              <th style="width:70px;"></th>
           </tr>
       </thead>
       <tbody id="table-body">
       </tbody>
     </table>
-    </div>
   </div>
 </div>
 <!-- SCRIPTS HERE!! -->
-<script src=".admin/.js\inventory\category.js"></script>
+<script src=".admin/.js\inventory\barcode.js"></script>
 <!-- END!! -->
